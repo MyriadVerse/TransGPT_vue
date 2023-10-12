8bfbfdf7740c88985f03ae9940ad3245e42ca86b
Idleman02 <mrsunyk02@gmail.com>
2023-10-13 01:23:19 +0800

    Add a declaration that treats vue as a module

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// Declare that vue is a module
+// used to solve vscode errors in index.ts
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
